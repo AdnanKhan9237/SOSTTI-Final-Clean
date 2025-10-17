88747af017c885caf85339f4e77497109733a874
AdnanKhan9237 <adnanwahid9237@gmail.com>
2025-10-17 10:47:36 +0500

    Update components.js

--- js/components.js
+++ js/components.js
@@ -354,7 +354,6 @@ document.addEventListener("DOMContentLoaded", () => {
             left: 0;
             width: 100%;
             height: 100%;
-            background-color: #ffffff;
             backdrop-filter: blur(3px);
             display: flex;
             justify-content: center;
